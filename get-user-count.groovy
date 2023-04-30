0e650c612653e4f1bb3aa83098136dcd906e2a0e
govindarajanv-g247 <118343889+govindarajanv-g247@users.noreply.github.com>
2023-04-30 15:33:13 +0530

    Update get-user-count.groovy

--- get-user-count.groovy
+++ get-user-count.groovy
@@ -1,9 +1,7 @@
-import com.cloudbees.hudson.plugins.folder.Folder
-import groovy.transform.Field
-import hudson.model.Job
 import hudson.model.User
-import jenkins.model.Jenkins
-import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+User.getAll().each { user ->
+println user.getId()}
 
 
 total_users = User.getAll().size()
+
